c9a1b5b3ce7a3cabfb680c12fbd71113110c0486
Aleksandar1609 <aleksandarjevtic16@gmail.com>
2023-10-27 09:09:19 +0200

    Added changes

---
+++ js/config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBdsKfmCg6K3-JtgAC7nSM7zBUTG_HhQz4",
+  authDomain: "dingdong-77f20.firebaseapp.com",
+  databaseURL: "https://dingdong-77f20-default-rtdb.firebaseio.com",
+  projectId: "dingdong-77f20",
+  storageBucket: "dingdong-77f20.appspot.com",
+  messagingSenderId: "291969162724",
+  appId: "1:291969162724:web:4fee4a1fdd0752c1377bf1",
+  measurementId: "G-EJ7XF0N738"
+  };
+
+  firebase.initializeApp(firebaseConfig);
